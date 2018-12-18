b2d32490b5086309f4781f512046e446de847014
P31umi <oluwapelumi.adeyemi@konga.com>
2018-12-18 10:31:31 +0100

    excerise 2 solution

---
+++ Exercise 2/assets/js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+ * html5-gulp-boilerplate
+ * 
+ * 
+ * @author Oluwapelumi Adeyemi
+ * @version 1.0.0
+ * Copyright 2018. ISC licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    function sendform () {
+      $("#form").hide();
+    }
+  });
+
+})(jQuery, window, document);
